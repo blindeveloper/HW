d5edb6c62784f3ff398afb06c7dedf278616da33
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-11-13 18:38:20 +0100

    async added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 
 // 1. Print numbers from 1 to 100 to the console, but for each number also print a random word using the function `getRandomWordSync`.
 const app = {
-  counter:0,
+  counter:1,
   until:100,
   getStringToprint: (counter, word) => `${counter}: ${word ? word : getRandomWordSync({ withErrors: false })}`,
   getWordByIndex: counter => {
@@ -17,25 +17,31 @@ const app = {
     return newWord
   },
   step1: () => {
-    while(app.counter < app.until) {
-      app.counter++
+    while(app.counter <= app.until) {
       console.log(app.getStringToprint(app.counter, null));
+      app.counter++
    }
   },
   step2: () => {
-    while(app.counter < app.until) {
-      app.counter++
+    while(app.counter <= app.until) {
       let fizzBuzzWord = app.getWordByIndex(app.counter)
       console.log(app.getStringToprint(app.counter, fizzBuzzWord));
+      app.counter++
    }
+  },
+  step3_1: async function() {
+    for (; app.counter <= app.until; app.counter++) {
+        await getRandomWord().then(randomWord => {
+          console.log(app.getStringToprint(app.counter, randomWord))
+        }, reject => {
+          console.log('reject: ', reject);
+        })
+    }
   }
 }
-getRandomWord().then(resolve => {
-  console.log('resolve: ', resolve);
-}, reject => {
-  console.log('reject: ', reject);
-})
+
 // app.step1()
 // app.step2()
+app.step3_1()
 // YOUR CODE HERE
 
